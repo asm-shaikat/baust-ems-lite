95fd321c5c49015af9467c4111cdf8421c67d530
Abu Shadat Shaikat <asmshaikat1952@gmail.com>
2022-09-05 11:07:19 +0600

    add students ui added

--- resources/views/Recruiter/add-student.blade.php
+++ resources/views/Recruiter/add-student.blade.php
@@ -4,22 +4,58 @@
 @section('title','Recruiter')
 
 @section('content')
-<div class="div_add">
-  <form action="" method="post">
-    <label for="fname">First Name</label>
-    <input class="input_text" type="text" id="fname" name="firstname" placeholder="Your name..">
-
-    <label for="lname">Last Name</label>
-    <input class="input_text" type="text" id="lname" name="lastname" placeholder="Your last name..">
-
-    <label for="country">Country</label>
-    <select class="input_select" id="country" name="country">
-      <option value="australia">Australia</option>
-      <option value="canada">Canada</option>
-      <option value="usa">USA</option>
-    </select>
-  
-    <input class="input_submit" type="submit" value="Submit">
-  </form>
+<!-- component -->
+<!-- This is an example component -->
+<div class="max-w-2xl mx-auto">
+<!-- component -->
+<div class="text-center mt-24">
+      <div class="text-sm  text-blue-500"> 
+         <h2>Register a new students</h2>
+</div>
+</div>
+<div class="flex justify-center my-2 mx-4 md:mx-0">
+   <form class="w-full max-w-xl bg-white rounded-lg shadow-md p-6">
+      <div class="flex flex-wrap -mx-3 mb-6">
+         <div class="w-full md:w-full px-3 mb-6">
+            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for='Password'>Email address</label>
+            <input class="appearance-none block w-full bg-white text-gray-900 font-medium border border-gray-400 rounded-lg py-3 px-3 leading-tight focus:outline-none" type='email'  required>
+         </div>
+         <div class="w-full md:w-full px-3 mb-6">
+            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for='Password'>Full name</label>
+            <input class="appearance-none block w-full bg-white text-gray-900 font-medium border border-gray-400 rounded-lg py-3 px-3 leading-tight focus:outline-none" type='email'  required>
+         </div>
+         <div class="w-full md:w-full px-3 mb-6">
+            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for='Password'>Address</label>
+            <input class="appearance-none block w-full bg-white text-gray-900 font-medium border border-gray-400 rounded-lg py-3 px-3 leading-tight focus:outline-none" type='email'  required>
+         </div>
+         <div class="w-full md:w-full px-3 mb-6">
+            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for='Password'>Fathers Name</label>
+            <input class="appearance-none block w-full bg-white text-gray-900 font-medium border border-gray-400 rounded-lg py-3 px-3 leading-tight focus:outline-none" type='email'  required>
+         </div>
+         <div class="w-full md:w-full px-3 mb-6">
+            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for='Password'>Mothers Name</label>
+            <input class="appearance-none block w-full bg-white text-gray-900 font-medium border border-gray-400 rounded-lg py-3 px-3 leading-tight focus:outline-none" type='email'  required>
+         </div>
+         <div class="w-full md:w-full px-3 mb-6">
+            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for='Password'>Permanent Address</label>
+            <input class="appearance-none block w-full bg-white text-gray-900 font-medium border border-gray-400 rounded-lg py-3 px-3 leading-tight focus:outline-none" type='email'  required>
+         </div>
+         <div class="w-full md:w-full px-3 mb-6">
+            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for='Password'>Present address</label>
+            <input class="appearance-none block w-full bg-white text-gray-900 font-medium border border-gray-400 rounded-lg py-3 px-3 leading-tight focus:outline-none" type='email'  required>
+         </div>
+         <div class="w-full md:w-full px-3 mb-6">
+            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for='Password'>Password</label>
+            <input class="appearance-none block w-full bg-white text-gray-900 font-medium border border-gray-400 rounded-lg py-3 px-3 leading-tight focus:outline-none" type='password' required>
+         </div>
+         <div class="w-full flex items-center justify-between px-3 mb-3 ">
+          
+         </div>
+         <div class="w-full md:w-full px-3 mb-6">
+            <button class="appearance-none block w-full bg-blue-600 text-gray-100 font-bold border border-gray-200 rounded-lg py-3 px-3 leading-tight hover:bg-blue-500 focus:outline-none focus:bg-white focus:border-gray-500">ADD Student</button>
+         </div>
+         
+      </div>
+   </form>
 </div>
 @endsection
