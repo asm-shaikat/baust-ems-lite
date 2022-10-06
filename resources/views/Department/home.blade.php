7e681c0e58569468ef3ca44f9ef186123c16b5d7
mdalarmansorker <mdalarmansorker@gmail.com>
2022-10-06 19:41:50 +0600

    Department:dashboard:pdf

--- resources/views/Department/home.blade.php
+++ resources/views/Department/home.blade.php
@@ -2,5 +2,9 @@
 @section('title','Home')
 @extends('Department.navbar')
 @section('content')
-    <p>garbage</p>
+<h1>Routine Demo</h1>
+    <iframe 
+    src="/upload/B_Sc_Engineering_in_CSE_Syllabus_2019.pdf" width="1000px" height="600px">
+    </iframe>
+
 @endsection
